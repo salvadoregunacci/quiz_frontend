6da0c027cab2cc54fcdd73713edc5df097b68604
Salvadore <salvadoregunacci@gmail.com>
2025-03-04 16:18:08 +0200

    изменил url бека

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://quiz-backend-production-4bee.up.railway.app/",
+    baseURL: "https://quiz-backend-gypq.onrender.com",
 });
 
 const token = window.localStorage.getItem("token");
